8d87fa6a2337aaade97885ef03b6eb57f25d3c2b
Abhishek <MrPhantom@gmail.com>
2021-03-07 19:35:19 -0200

    Published website

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+    var type= new Typed('#typed',{
+        stringsElement: '#typed-string',
+        typeSpeed:100,
+    cursorSpeed: 500,
+    startDelay: 1500,
+    backSpeed: 100,
+    backdelay: 500,
+    loop: true,
+    loopCount: 100,
+    showCursor: false,
+    cursorChar: "|",
+    attr: null,
+    contentType: 'html',
+    callback: function(){ },
+    preStringTyped: function(){ },
+    oneStringTyped: function(){ },
+    resetCallback: function(){ }
+});
